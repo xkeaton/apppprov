a0b95cebb456734af6db7ca9decab07057b23cfe
gracian kenyi mamani tapia <gmamani@rivercon.com>
2022-01-04 11:36:56 +0000

    1

--- ui5apppprov/webapp/controller/Vista_Administrador_Usuarios.controller.js
+++ ui5apppprov/webapp/controller/Vista_Administrador_Usuarios.controller.js
@@ -50,7 +50,7 @@ sap.ui.define([
 			var oView = this.getView();
 			var oModel = oView.getModel("myParam");
 			oView.setModel(oModel);
-			// Tablas
+			// Tablas1
 			this.varTableURL = oModel.getProperty("/listTablasOData/clistTablasODataURL");
 			this.varTableDocument = oModel.getProperty("/listTablasOData/clistTablasODataDocument");
 			this.varTableT_CEN = oModel.getProperty("/listTablasOData/clistTablasODataT_CEN");
